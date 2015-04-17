dbb8f1b2820d72e627a2c3c14da53d17ef8b48dd
Kostya Zavgorodniy <k.zavgor@gmail.com>
2015-04-17 21:54:58 +0300

    Initialize

---
+++ src/base/BaseActions.java
@@ -0,0 +1,14 @@
+package base;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.io.File;
+
+public class BaseActions {
+
+
+
+}
---
+++ src/tests/Test_test.java
@@ -0,0 +1,10 @@
+package tests;
+
+/**
+ * Created by Завгородній on 16.04.2015.
+ */
+public class Test_test {
+    public static void main(String [] args) {
+        System.out.println(System.getProperty("user.dir") + "\\lib\\driver");
+    }
+}
